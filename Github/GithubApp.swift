ea771cea50afc5bf2116971021706fdad0ade955
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-04-20 19:31:08 +0600

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Sheikh Bayazid on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
